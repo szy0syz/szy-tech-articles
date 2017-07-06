0a020cbddadf9607abc1f284c7556e4005c1b0bb
szy0syz <szy0syz@gmail.com>
2017-07-06 10:50:41 +0800

    updated config.js alias

--- config.js
+++ config.js
@@ -20,7 +20,12 @@ window.$docsify = {
       '/notes/_sidebar.md': '/_sidebar.md',
       '/OS/_sidebar.md': '/_sidebar.md',
       '/notes/learing/_sidebar.md': '/_sidebar.md',
-      'notes/reading/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/9787111376613/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/9787115335500/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/9787115352460/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/9787115380333/_sidebar.md': '/_sidebar.md',
+      '/notes/reading/9787121276576/_sidebar.md': '/_sidebar.md',
       '/Knowledge/FullStack/_sidebar.md': '/_sidebar.md'
     },
     plugins: [
