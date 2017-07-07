8ad47d9ee9ef69d52aa65bd1aed18e40993554d6
Jerry Shi <szy0syz@gmail.com>
2017-07-07 09:37:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,6 +21,7 @@ window.$docsify = {
       '/OS/_sidebar.md': '/_sidebar.md',
       '/notes/learning/_sidebar.md': '/_sidebar.md',
       '/notes/learning/css/_sidebar.md': '/_sidebar.md',
+      '/notes/learning/js/_sidebar.md': '/_sidebar.md',
       '/notes/reading/_sidebar.md': '/_sidebar.md',
       '/notes/reading/9787111376613/_sidebar.md': '/_sidebar.md',
       '/notes/reading/9787115335500/_sidebar.md': '/_sidebar.md',
