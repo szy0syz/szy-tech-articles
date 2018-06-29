11ee17f606826fe5ee861eacc9f0f623312c4ea0
szy0syz <szy0syz@gmail.com>
2018-06-29 13:49:23 +0800

    update 更新worker脚本；新增ejs

--- worker.js
+++ worker.js
@@ -1,25 +1,44 @@
-const { resolve } = require('path')
+const ejs = require('ejs')
 const glob = require('glob')
-const _ = require('lodash')
-const R = require('ramda')
 const fs = require('fs')
-const readline = require('linebyline')
 
-// const list = glob.sync(resolve(__dirname, './notes/reading/**/*.md'))
-const list = glob.sync('./notes/learning/**/*.md').forEach(i => {
-  if (i) {
-    fs.readFile(i, 'utf8', (err, data) => {
-      if (err) { throw new Error(err) }
-      else {
-        try {
-          const firstLine = data.split('\n')[0]
-          if (firstLine[0] !== '#') {
-            console.log('文件:' + i, '存在格式问题')
-          }
-        } catch (error) {
-          console.error(error)
+const pathList = ['./reading', './learning/fe']
+
+function getContents(p) {
+  let list = []
+
+  const tpl = `# CONTENTS
+<% for(let i=0; i < data.length; i++) { %>
+* [<%= data[i].name %>](<%= data[i].path %>)
+<% } %>`
+
+  ~(async () => {
+    let ll = glob.sync(p + '/**/*.md').filter(i => i.indexOf('index') < 0)
+
+    await Promise.all(
+      ll.map(async path => {
+        const data = await fs.readFileSync(path, 'utf8')
+        const firstLine = data.split('\n')[0]
+        if (firstLine[0] !== '#') {
+          console.log('文件:' + path, '存在格式问题')
+        } else {
+          list.push({
+            name: firstLine.replace('# ', ''),
+            path: path.replace('.', '')
+          })
         }
-      }
+      })
+    )
+
+    const rr = ejs.render(tpl, {
+      data: list
     })
-  }
-})
+
+    fs.writeFile(p + '/index.md', rr, err => {
+      if (err) console.error(err)
+      else console.info('写入 ' + p + '/index.md 成功')
+    })
+  })()
+}
+
+pathList.forEach(getContents)
